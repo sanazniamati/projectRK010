eccfa5b16510f29eb0c417510c04e2afcf2d7242
sanaz-niamati <sanaz.niamati@yahoo.com>
2022-06-19 10:36:52 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -1,37 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Line } from "react-konva";
 
 const App = () => {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Rect
-          x={20}
-          y={20}
-          width={100}
-          height={50}
-          fill="green"
-          stroke="black"
-          strokWidth={4}
-        />
-        <Rect
-          x={150}
-          y={40}
-          width={100}
-          height={50}
-          fill="red"
-          shadowBlur={10}
-          stroke="black"
-          cornerRadius={10}
-        />
-        <Rect
-          x={50}
-          y={120}
-          width={100}
-          height={100}
-          fill="blue"
-          cornerRadius={[0, 10, 20, 30]}
+        <Line
+          points={[23, 20, 23, 160, 70, 93, 150, 109, 290, 139, 270, 93]}
+          fill={"#00D2FF"}
+          stroke={"black"}
+          strokeWidth={5}
+          closed
+          tension={0.3}
         />
       </Layer>
     </Stage>
